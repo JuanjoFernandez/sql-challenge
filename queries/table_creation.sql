2068d0bdb288b82651f23066d32164d46e960d3c
Juanjo <juanjo_fdez@yahoo.com.mx>
2021-05-13 10:09:45 -0500

    Fixes foreign keys relations

--- queries/table_creation.sql
+++ queries/table_creation.sql
@@ -3,39 +3,47 @@
 -- Child tables
 drop table dept_emp;
 drop table salaries;
-drop table titles;
 drop table dept_manager;
 
 -- Parent tables
 drop table departments;
 drop table employees;
+drop table titles;
 
 -- Creating all tables
+create table titles (
+	title_id varchar(4) primary key,
+	title varchar (30));
 
-create table employees (emp_no int primary key,
-						emp_title_id varchar(5),
-						birth_date varchar(10),
-						first_name varchar(30),
-						last_name varchar(30),
-						sex varchar (1),
-						hire_date varchar(10));
+create table employees (
+	emp_no int primary key,
+	emp_title_id varchar(5),
+	birth_date varchar(10),
+	first_name varchar(30),
+	last_name varchar(30),
+	sex varchar (1),
+	hire_date varchar(10),
+	constraint fk_titles foreign key(emp_title_id) references titles(title_id));
 
-create table departments (dept_no int primary key,
-				   dept_name varchar(30));
+create table salaries (
+	emp_no int,
+	salary int,
+	constraint fk_employee foreign key(emp_no) references employees(emp_no));
 
-create table salaries (emp_no int,
-					  salary int,
-					  constraint fk_employee foreign key(emp_no) references employees(emp_no));
+create table departments (
+	dept_no varchar(4) primary key,
+	dept_name varchar(30));
 
-create table titles (title_id int primary key,
-					title varchar (30));
+create table dept_emp (
+	emp_no int,dept_no varchar(4),
+	constraint fk_employee foreign key(emp_no) references employees(emp_no),
+	constraint fk_department foreign key(dept_no) references departments(dept_no));
 
-create table dept_manager (depto_no int,
-						  emp_no int,
-						  constraint fk_department foreign key(emp_no) references departments(dept_no),
-						  constraint fk_employee foreign key(emp_no) references employees(emp_no));
+create table dept_manager (
+	dept_no varchar(4),
+	emp_no int,
+	constraint fk_department foreign key(dept_no) references departments(dept_no),
+	constraint fk_employee foreign key(emp_no) references employees(emp_no));
 
-create table dept_emp (emp_no int,
-				dept_no int,
-				constraint fk_employee foreign key(emp_no) references employees(emp_no),
-				constraint fk_department foreign key(emp_no) references departments(dept_no));
+
+
