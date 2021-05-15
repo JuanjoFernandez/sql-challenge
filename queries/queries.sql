0e9b950d36a3a5c2fa390e8ce79f397bab5b7164
Juanjo <juanjo_fdez@yahoo.com.mx>
2021-05-15 15:05:08 -0500

    Adds final quieries

--- queries/queries.sql
+++ queries/queries.sql
@@ -33,6 +33,31 @@ from employees
 	inner join dept_emp on
 	dept_emp.emp_no = employees.emp_no
 	inner join departments on
-	dept_emp.dept_no = departments.dept_name;
+	dept_emp.dept_no = departments.dept_no;
 
+-- -- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+select first_name, last_name, sex from employees
+	where first_name = 'Hercules' and last_name like 'B%';
 
+-- -- List all employees in the Sales department, including their employee number, last name, first name, and department name.
+select employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+from employees
+	inner join dept_emp on
+	dept_emp.emp_no = employees.emp_no
+	inner join departments on
+	dept_emp.dept_no = departments.dept_no
+-- Filtering by id in case sales changes name
+where departments.dept_no = 'd007';
+
+-- -- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+select employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+from employees
+	inner join dept_emp on
+	dept_emp.emp_no = employees.emp_no
+	inner join departments on
+	dept_emp.dept_no = departments.dept_no
+-- Filtering by id in case sales changes name
+where departments.dept_no = 'd007' or departments.dept_no = 'd005';
+
+-- -- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+select last_name, count(last_name) as LNCount from employees group by last_name order by LNCount desc;
